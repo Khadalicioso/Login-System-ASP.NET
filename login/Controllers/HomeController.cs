3095a093a237f29162b8f4ef8ad7bb5013bb8387
Khadalicioso <akosikhada@gmail.com>
2024-10-21 19:13:24 +0800

    Add project files.

---
+++ login/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using login.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace login.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+        public IActionResult Success()
+        {
+            return View();
+        }
+        public IActionResult Testing()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
